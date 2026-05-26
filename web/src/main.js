2bea94be08887c0c0e96e9775902feb521e375cd
gaowenhao.0416 <gaowenhao.0416@bytedance.com>
2026-05-26 20:24:56 +0800

    feat: switch frontend to Vue 3 + Vite with vue-router

---
+++ web/src/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Login from './views/Login.vue'
+import Home from './views/Home.vue'
+import './style.css'
+
+const routes = [
+  { path: '/', component: Home, meta: { requiresAuth: true } },
+  { path: '/login', component: Login },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else if (to.path === '/login' && token) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+createApp(App).use(router).mount('#app')
